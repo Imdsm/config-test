94409b50c4a3fe49706346d8927b2c1d419830b2
Adam K Dean <adam@amberfish.co.uk>
2014-11-21 12:11:17 +0000

    Fixed issue. res.end is not config.log. Oops.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function(req, res) {
     if (req.url === '/') {
         res.writeHead(200, {'Content-Type': 'text/plain' });
-        res.end('EXAMPLE_CONFIG_VAR:', process.env.EXAMPLE_CONFIG_VAR);
+        res.end('EXAMPLE_CONFIG_VAR: ' + process.env.EXAMPLE_CONFIG_VAR);
     } else {
         res.writeHead(404, {'Content-Type': 'text/plain' });
         res.end('Not found');
